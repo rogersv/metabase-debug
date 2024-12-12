96d9a934417163cf14b5976273b49ce252cf016f
Roger Svahn <roger.svahn@lime.tech>
2024-12-12 14:44:23 +0100

    feat: test segment mapper (#2)

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import os
+import shutil
+import tempfile
+from pathlib import Path
 
 import click
 from dotenv import load_dotenv
+from limepkg_metabase.serialization.file_operations import (
+    create_modified_tarball,
+    extract_tarball,
+)
 
 import util
 
-load_dotenv()
+load_dotenv(override=True)
 
 LAMBDA_PRODUCTION_API_KEY = os.getenv("LAMBDA_PRODUCTION_API_KEY")
 LAMBDA_PRODUCTION_ENDPOINT = os.getenv("LAMBDA_PRODUCTION_ENDPOINT")
@@ -26,12 +33,52 @@ METABASE_TESTING_ADMIN_USERNAME = os.getenv("METABASE_TESTING_ADMIN_USERNAME")
 METABASE_TESTING_ADMIN_PASSWORD = os.getenv("METABASE_TESTING_ADMIN_PASSWORD")
 METABASE_TESTING_METABASE_URL = os.getenv("METABASE_TESTING_METABASE_URL")
 
-METABASE_LIME_CLOUD_ADMIN_USERNAME = os.getenv("METABASE_LIME_CLOUD_ADMIN_USERNAME")
-METABASE_LIME_CLOUD_ADMIN_PASSWORD = os.getenv("METABASE_LIME_CLOUD_ADMIN_PASSWORD")
-METABASE_LIME_CLOUD_METABASE_URL = os.getenv("METABASE_LIME_CLOUD_METABASE_URL")
+METABASE_LIME_CLOUD_ADMIN_USERNAME = os.getenv(
+    "METABASE_LIME_CLOUD_ADMIN_USERNAME"
+)
+METABASE_LIME_CLOUD_ADMIN_PASSWORD = os.getenv(
+    "METABASE_LIME_CLOUD_ADMIN_PASSWORD"
+)
+METABASE_LIME_CLOUD_METABASE_URL = os.getenv(
+    "METABASE_LIME_CLOUD_METABASE_URL"
+)
 
+METABASE_LIME_CLOUD_DEV_ADMIN_USERNAME = os.getenv(
+    "METABASE_LIME_CLOUD_DEV_ADMIN_USERNAME"
+)
+METABASE_LIME_CLOUD_DEV_ADMIN_PASSWORD = os.getenv(
+    "METABASE_LIME_CLOUD_DEV_ADMIN_PASSWORD"
+)
+METABASE_LIME_CLOUD_DEV_METABASE_URL = os.getenv(
+    "METABASE_LIME_CLOUD_DEV_METABASE_URL"
+)
+
+TEST_IMPORT_APP_ID = os.getenv("TEST_IMPORT_APP_ID")
 TEST_IMPORT_APP_USERNAME = os.getenv("TEST_IMPORT_APP_USERNAME")
 TEST_IMPORT_APP_PASSWORD = os.getenv("TEST_IMPORT_APP_PASSWORD")
+
+LIME_BI_CREDENTIALS = {
+    "production": {
+        "admin_username": METABASE_PRODUCTION_ADMIN_USERNAME,
+        "admin_password": METABASE_PRODUCTION_ADMIN_PASSWORD,
+        "metabase_url": METABASE_PRODUCTION_METABASE_URL,
+    },
+    "testing": {
+        "admin_username": METABASE_TESTING_ADMIN_USERNAME,
+        "admin_password": METABASE_TESTING_ADMIN_PASSWORD,
+        "metabase_url": METABASE_TESTING_METABASE_URL,
+    },
+    "lime_cloud": {
+        "admin_username": METABASE_LIME_CLOUD_ADMIN_USERNAME,
+        "admin_password": METABASE_LIME_CLOUD_ADMIN_PASSWORD,
+        "metabase_url": METABASE_LIME_CLOUD_METABASE_URL,
+    },
+    "lime_cloud_dev": {
+        "admin_username": METABASE_LIME_CLOUD_DEV_ADMIN_USERNAME,
+        "admin_password": METABASE_LIME_CLOUD_DEV_ADMIN_PASSWORD,
+        "metabase_url": METABASE_LIME_CLOUD_DEV_METABASE_URL,
+    },
+}
 
 
 @click.group()
@@ -72,48 +119,87 @@ def load_applications(environment):
     help="The environment to use (production or testing)",
 )
 def test_export(environment):
-    lime_bi_credentials = {
-        "production": {
-            "admin_username": METABASE_PRODUCTION_ADMIN_USERNAME,
-            "admin_password": METABASE_PRODUCTION_ADMIN_PASSWORD,
-            "metabase_url": METABASE_PRODUCTION_METABASE_URL,
-        },
-        "testing": {
-            "admin_username": METABASE_TESTING_ADMIN_USERNAME,
-            "admin_password": METABASE_TESTING_ADMIN_PASSWORD,
-            "metabase_url": METABASE_TESTING_METABASE_URL,
-        },
-    }
-    util.test_export_for_apps(lime_bi_credentials[environment], environment)
+    util.test_export_for_apps(LIME_BI_CREDENTIALS, environment)
 
 
 @click.command()
 def import_collection():
     util.import_collection_to_lime_bi(
-        app_id="0b6ea78da60d4aae817a13c304ac4fb5",
+        app_id="d50c43e32a4447a0be647d3d2079115a",
         app_information={
             "app_user_username": TEST_IMPORT_APP_USERNAME,
             "app_user_password": TEST_IMPORT_APP_PASSWORD,
             "lime_bi_config": {
-                "unique_identifier": "corem-test",
-                "group_name": "corem-test",
-                "group_id": 62,
-                "database_id": 60,
-                "collection_id": 291,
-                "is_initialized": True
+                "unique_identifier": "roger-test-db-image",
+                "group_name": "roger-test-db-image",
+                "group_id": 16,
+                "database_id": 22,
+                "collection_id": 42,
+                "is_initialized": True,
             },
         },
         lime_bi_credentials={
-            "admin_username": METABASE_LIME_CLOUD_ADMIN_USERNAME,
-            "admin_password": METABASE_LIME_CLOUD_ADMIN_PASSWORD,
-            "metabase_url": METABASE_LIME_CLOUD_METABASE_URL,
+            "admin_username": METABASE_LIME_CLOUD_DEV_ADMIN_USERNAME,
+            "admin_password": METABASE_LIME_CLOUD_DEV_ADMIN_PASSWORD,
+            "metabase_url": METABASE_LIME_CLOUD_DEV_METABASE_URL,
         },
+    )
+
+
+@click.command()
+def remove_segments():
+    export_tarfile_path = "export.tar.gz"
+
+    with tempfile.TemporaryDirectory():
+        temp_dir_database = Path(tempfile.mkdtemp())
+        extract_tarball(export_tarfile_path, temp_dir_database)
+        util.remove_all_segments_in_files(temp_dir_database)
+
+    new_tarball_path = create_modified_tarball(temp_dir_database)
+    shutil.move(new_tarball_path, "modified_export.tar.gz")
+
+
+@click.command()
+def test_replace_segments():
+
+    apps = util.load_application_data("testing")
+    source_app_id = "89cc050582504c248364ca7bf0365d00"
+    source_database_id = 89
+    destination_database_id = 22
+
+    source_app = apps[source_app_id]
+
+    client_factory_old = util.MetabaseCloudClientFactory(
+        source_app_id,
+        LIME_BI_CREDENTIALS["testing"]["admin_username"],
+        LIME_BI_CREDENTIALS["testing"]["admin_password"],
+        LIME_BI_CREDENTIALS["testing"]["metabase_url"],
+        app_user_username=source_app["app_user_username"],
+        app_user_password=source_app["app_user_password"],
+    )
+
+    client_factory_new = util.MetabaseCloudClientFactory(
+        TEST_IMPORT_APP_ID,
+        LIME_BI_CREDENTIALS["lime_cloud_dev"]["admin_username"],
+        LIME_BI_CREDENTIALS["lime_cloud_dev"]["admin_password"],
+        LIME_BI_CREDENTIALS["lime_cloud_dev"]["metabase_url"],
+        app_user_username=TEST_IMPORT_APP_USERNAME,
+        app_user_password=TEST_IMPORT_APP_PASSWORD,
+    )
+
+    util.replace_segments(
+        source_client_factory=client_factory_old,
+        destination_client_factory=client_factory_new,
+        source_database_id=source_database_id,
+        destination_database_id=destination_database_id,
     )
 
 
 cli.add_command(load_applications)
 cli.add_command(test_export)
 cli.add_command(import_collection)
+cli.add_command(remove_segments)
+cli.add_command(test_replace_segments)
 
 if __name__ == "__main__":
     cli()
